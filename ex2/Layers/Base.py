7b0e349cb48652e6bd3bcff4e87c1a7ed99d9d47
Lama <81018335+lamara11@users.noreply.github.com>
2023-07-09 00:35:42 +0200

    Delete ex2 directory

--- ex2/Layers/Base.py
+++
@@ -1,9 +0,0 @@
-class BaseLayer:
-    """
-       Simple base layer with argument if the layer is trainable or not
-       """
-    def __init__(self):
-        self.trainable=False
-        self.weights=None
-
-
