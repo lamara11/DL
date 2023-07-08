a030b96a3a40c57fc3445a1f2a206ac039406f86
Lama <81018335+lamara11@users.noreply.github.com>
2023-07-09 00:32:40 +0200

    Update export_onnx.py

--- ex4/export_onnx.py
+++ ex4/export_onnx.py
@@ -2,10 +2,10 @@ import torch as t
 from trainer import Trainer
 import sys
 import torchvision as tv
-
+from model import ResNet
 epoch = int(sys.argv[1])
 #TODO: Enter your model here
-
+model = ResNet()
 crit = t.nn.BCELoss()
 trainer = Trainer(model, crit)
 trainer.restore_checkpoint(epoch)
